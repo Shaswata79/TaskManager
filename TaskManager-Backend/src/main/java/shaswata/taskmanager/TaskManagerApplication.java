95ba4e5bed5a3fbb73014352e759ac66dfe1811c
Shaswata79 <shaswatabhatt2030@gmail.com>
2021-05-17 20:14:30 +0600

    Initial commit of the backend application

---
+++ TaskManager-Backend/src/main/java/shaswata/taskmanager/TaskManagerApplication.java
@@ -0,0 +1,21 @@
+package shaswata.taskmanager;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class TaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
+	}
+
+}

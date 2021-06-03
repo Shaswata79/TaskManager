f8fcec82641464f5bf57866290662478460806c3
Shaswata79 <shaswatabhatt2030@gmail.com>
2021-06-03 18:35:46 +0600

    Configured Swagger to work with Spring Security

--- TaskManager-Backend/src/main/java/shaswata/taskmanager/swagger/SwaggerConfig.java
+++ TaskManager-Backend/src/main/java/shaswata/taskmanager/swagger/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package shaswata.taskmanager.swagger;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -7,20 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static String AUTHORIZATION_HEADER = "token";
+    public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
+
     @Bean
     public Docket swaggerApiConfig(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
@@ -36,6 +46,27 @@ public class SwaggerConfig {
                 .version("1.0")
                 .contact(new Contact("Shaswata Bhattacharyya", "https://github.com/Shaswata79", "shaswatabhatt2030@gmail.com"))
                 .build();
+    }
+
+
+    private ApiKey apiKey(){
+        return new ApiKey("jwt", AUTHORIZATION_HEADER, "header");
+    }
+
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("jwt", authorizationScopes));
     }
 
 
